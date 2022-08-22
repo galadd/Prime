00c6e53ab25d9949f736c05146e62990e8813192
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2022-08-22 13:32:18 +0100

    Added Prime token (PrimeX)

---
+++ contracts/PrimeX.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PrimeX is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("PrimeX", "PUX") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+/*
+    modifier deadlock {
+        require(block.timestamp > 24 hours, "");
+    }*/
+}
